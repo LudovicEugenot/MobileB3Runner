3bec6ee755582c5ccead3d4aa83132533ce74969
Luana-GD <lmtn2706@gmail.com>
2021-05-20 17:00:38 +0200

    tf did I do on this again? Anyway je merge

---
+++ B3MobileRunner/Assets/_Scripts/SoundScripts/SoundManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    private static SoundManager _instance;
+    public static SoundManager instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<SoundManager>();
+            }
+            return _instance;
+        }
+    }
+
+    private AudioSource[] gameSounds;
+
+    [SerializeField]
+    private AudioSource tap;
+    [SerializeField]
+    private AudioSource slash1;
+    [SerializeField]
+    private AudioSource slash2;
+
+}
